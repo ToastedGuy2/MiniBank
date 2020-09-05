249e5ab492b84196da86424a66eecc6eda7a08cd
Toastedguy2 <Kevinhs472@gmail.com>
2020-09-05 13:25:48 -0600

    Arquitecture ready

---
+++ MiniBank.Tests.Xunit/IWillNameItLater.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace MiniBank.Tests.Xunit
+{
+    public class IWillNameItLater
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
